cb6f97e6c356158fd0b5a48d1f314e86b9cc797d
Supun Prabodha <supunprabodha0915@gmail.com>
2025-03-19 11:38:11 +0530

    Updated feedback component and restructured folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from "./Components/Home/Home";
+import Home from "./Components/Feedback/Feedback";
 
 function App() {
   return (
